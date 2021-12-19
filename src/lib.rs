5889bc177e2a97491080dc174d3c316f115b84ec
inty <interruptinuse@gmail.com>
2021-12-19 14:59:04 +0000

    NtfsPathCollator: tests: Add Lorem ipsum, UAX #15

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+#[cfg(test)]
+use std::io::Cursor;
 use ntfs_upcase::NtfsUpcaseTable;
 use unicode_normalization::UnicodeNormalization;
 
@@ -7,6 +10,7 @@ pub struct NtfsPathCollator {
 }
 
 
+#[derive(Debug)]
 pub enum NtfsPathCollatorError {
 	DecodeUtf16Error(std::char::DecodeUtf16Error)
 }
@@ -25,4 +29,24 @@ impl NtfsPathCollator {
 			.collect::<Result<String>>()?;
 		Ok(upcased_decode.nfc().collect())
 	}
+}
+
+#[test]
+fn test_normalize_utf16_path() {
+	let mut upcase_ntfs3g = Cursor::new(ntfs_upcase::UPCASEDATA_NTFS3G);
+	let mut upcaseinfo_ntfs3g = Cursor::new(ntfs_upcase::UPCASEINFO_NTFS3G);
+	let upcase = NtfsUpcaseTable::try_parse_upcase_and_info_files(&mut upcase_ntfs3g, &mut upcaseinfo_ntfs3g).unwrap();
+	let collator = NtfsPathCollator { upcase };
+
+	assert_eq!(collator.normalize_utf16_path("Lorem ipsum").unwrap(), "LOREM IPSUM");
+	assert_eq!(collator.normalize_utf16_path("Wacław Sierpiński").unwrap(), "WACŁAW SIERPIŃSKI");
+
+	// UAX #15, fig. 3 (https://unicode.org/reports/tr15/)
+	assert_eq!(collator.normalize_utf16_path("\u{212B}").unwrap(), "\u{00C5}");
+	assert_eq!(collator.normalize_utf16_path("\u{2126}").unwrap(), "\u{03A9}");
+
+	// UAX #15, fig. 5
+	assert_eq!(collator.normalize_utf16_path("\u{1E69}").unwrap(), "\u{1E68}");
+	assert_eq!(collator.normalize_utf16_path("\u{1E0B}\u{0323}").unwrap(), "\u{1E0C}\u{0307}");
+	assert_eq!(collator.normalize_utf16_path("\u{0071}\u{0307}\u{0323}").unwrap(), "\u{0051}\u{0323}\u{0307}");
 }
